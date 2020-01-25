99fc03b27a955077a3d4e135e5051c6d5fe01acd
School2020 <paul.dibble3@gmail.com>
2020-01-25 15:31:14 -0800

    added some rooms

--- DC/Dungon_Crawler/Dungon_Crawler.cpp
+++ DC/Dungon_Crawler/Dungon_Crawler.cpp
@@ -1,11 +1,13 @@
-	#include "Dungon_Crawler.h"
+#include "Dungon_Crawler.h"
 
+void Run()
+{
 
-void Run() {
 	srand(time(NULL));
-	 Spawner s;
-	Player player;
-	// = createPlayer();
+
+	Spawner s;
+
+	Player player = createPlayer();
 
 	// player.addToInventory(s.CreateDevSword());
 
@@ -20,50 +22,108 @@ void Run() {
 	// player.addToInventory(s.CreateDevGloves(Light_ArmorClass));
 
 	// player.addToInventory(s.CreateDevPants(Light_ArmorClass));
-	player.addToInventory(s.CreateAxe(1,true));
-	player.addToInventory(s.CreateDagger(1,true));
+
+	player.addToInventory(s.CreateAxe(1, true));
+
+	player.addToInventory(s.CreateDagger(1, true));
+
 	Armor *a;
-	a=s.CreateBoots(1,Light_ArmorClass,true);
-	map<Effects_enum ,ActiveEffects> e;
-	e[NormalResistance_Effects]=(ActiveEffects(Resistance_EffectTypes,NormalResistance_Effects,25,10));
+
+	a = s.CreateBoots(1, Light_ArmorClass, true);
+
+	map<Effects_enum, ActiveEffects> e;
+
+	e[NormalResistance_Effects] = (ActiveEffects(Resistance_EffectTypes, NormalResistance_Effects, 25, 10));
+
 	a->setResistanceTypes(e);
+
 	player.addToInventory(a);
 
-	player.addToInventory(s.CreatePants(1,Light_ArmorClass,true));
-	player.addToInventory(s.CreateChest(1,Light_ArmorClass,true));
-	player.addToInventory(s.CreateGloves(1,Light_ArmorClass,true));
-	player.addToInventory(s.CreateHelmet(1,Light_ArmorClass,true));
+	player.addToInventory(s.CreatePants(1, Light_ArmorClass, true));
+
+	player.addToInventory(s.CreateChest(1, Light_ArmorClass, true));
+
+	player.addToInventory(s.CreateGloves(1, Light_ArmorClass, true));
+
+	player.addToInventory(s.CreateHelmet(1, Light_ArmorClass, true));
 
 	Floor f;
 
+	int i;
 
-	f.setRoom(createRoom0_0(),0,0);
-	f.setRoom(createRoom1_0(),1,0);
+	int j;
+
+	for (i = 1; i < 9; i++)
+	{
+
+		for (j = 1; j < 9; j++)
+		{
+
+			f.setRoom(createDefaultRoom(), i, j);
+		}
+	}
+
+	for (i = 1; i < 9; i++)
+	{
+
+		f.setRoom(createLeftRoom(), i, 0);
+	}
+
+	for (i = 1; i < 9; i++)
+	{
+
+		f.setRoom(createRightRoom(), i, 9);
+	}
+
+	for (i = 1; i < 9; i++)
+	{
+
+		f.setRoom(createUpRoom(), 0, i);
+	}
+
+	for (i = 1; i < 9; i++)
+	{
+
+		f.setRoom(createDownRoom(), 9, i);
+	}
+
+	f.setRoom(createRoom0_0(), 0, 0);
+
+	f.setRoom(createRoom9_0(), 9, 0);
+
+	f.setRoom(createRoom9_9(), 9, 9);
+
+	f.setRoom(createRoom0_9(), 0, 9);
+
 	f.setPlayer(&player);
+
 	f.NavigateFloor();
-
-
-
 }
 
-Player createPlayer() {
+Player createPlayer()
+{
+
 	Player ret;
 
 	string name;
+
 	cout << "choose a name:" << endl;
-	cin>> name;
+
+	cin >> name;
 
 	ret.setName(name);
 
-
 	int points = 10;
+
 	int input = 0;
+
 	while (points != 0)
+
 	{
 
-	cout << "you have " << points << " remaining what would you like to upgrade?" << endl;
-		ret.addSkillPoint();
+		cout << "you have " << points << " remaining what would you like to upgrade?" << endl;
 
+		ret.addSkillPoint();
 
 		points--;
 	}
@@ -71,41 +131,396 @@ Player createPlayer() {
 	return ret;
 }
 
+Room createRoom0_0()
+{
 
-Room createRoom0_0(){
+	Room ret;
+
+	ret.PlacePlayer(1, 1);
+
+	ret.setMaxX(20);
+
+	ret.setMaxY(10);
+
+	//Top Wall
+
+	ret.addWalls(0, 0, 0, 9);
+
+	//Left Wall
+
+	ret.addWalls(1, 0, 19, 0);
+
+	//Buttom Wall
+
+	ret.addWalls(19, 1, 19, 5);
+
+	ret.addDownDoor(19, 6);
+
+	ret.addWalls(19, 7, 19, 9);
+
+	//Right Wall
+
+	ret.addWalls(1, 9, 4, 9);
+
+	ret.addRightDoor(5, 9);
+
+	ret.addWalls(6, 9, 18, 9);
+
+	return ret;
+}
+
+Room createRoom1_0()
+{
+
+	Room ret;
+
+	ret.setMaxX(10);
+
+	ret.setMaxY(20);
+
+	ret.addUpDoor(0, 4);
+
+	ret.addWalls(0, 0, 0, 3);
+
+	ret.addWalls(0, 4, 0, 19);
+
+	ret.addWalls(0, 0, 9, 0);
+
+	ret.addWalls(9, 0, 9, 19);
+
+	ret.addWalls(9, 0, 9, 19);
+
+	return ret;
+}
+
+Room createRoom0_1()
+{
+}
+
+Room createDefaultRoom()
+{
+
+	Room ret;
+
+	ret.setMaxX(10);
+
+	ret.setMaxY(10);
+
+	//Top Wall
+
+	ret.addWalls(0, 0, 0, 4);
+
+	ret.addUpDoor(0, 5);
+
+	ret.addWalls(0, 6, 0, 9);
+
+	//Left Wall
+
+	ret.addWalls(0, 0, 4, 0);
+
+	ret.addLeftDoor(5, 0);
+
+	ret.addWalls(6, 0, 9, 0);
+
+	//Bottom Wall
+
+	ret.addWalls(9, 0, 9, 4);
+
+	ret.addDownDoor(9, 5);
+
+	ret.addWalls(9, 6, 9, 9);
+
+	//Right Wall
+
+	ret.addWalls(0, 9, 4, 9);
+
+	ret.addRightDoor(5, 9);
+
+	ret.addWalls(6, 9, 9, 9);
+
+	ret.setStartingEnemyAmount(1);
+
+	return ret;
+}
+
+Room createLeftRoom()
+{
+
+	Room ret;
+
+	ret.setMaxX(10);
+
+	ret.setMaxY(10);
+
+	//Top Wall
+
+	ret.addWalls(0, 0, 0, 4);
+
+	ret.addUpDoor(0, 5);
+
+	ret.addWalls(0, 6, 0, 9);
+
+	//Left Wall
+
+	ret.addWalls(0, 0, 9, 0);
+
+	//Bottom Wall
+
+	ret.addWalls(9, 0, 9, 4);
+
+	ret.addDownDoor(9, 5);
+
+	ret.addWalls(9, 6, 9, 9);
+
+	//Right Wall
+
+	ret.addWalls(0, 9, 4, 9);
+
+	ret.addRightDoor(5, 9);
+
+	ret.addWalls(6, 9, 9, 9);
+
+	return ret;
+}
+
+Room createRightRoom()
+{
+
+	Room ret;
+
+	ret.setMaxX(10);
+
+	ret.setMaxY(10);
+
+	//top Wall
+
+	ret.addWalls(0, 0, 0, 4);
+
+	ret.addUpDoor(0, 5);
+
+	ret.addWalls(0, 6, 0, 9);
+
+	//Left Wall
+
+	ret.addWalls(0, 0, 4, 0);
+
+	ret.addLeftDoor(5, 0);
+
+	ret.addWalls(6, 0, 9, 0);
+
+	//Bottom Wall
+
+	ret.addWalls(9, 0, 9, 4);
+
+	ret.addDownDoor(9, 5);
+
+	ret.addWalls(9, 6, 9, 9);
+
+	ret.addWalls(0, 9, 9, 9);
+
+	return ret;
+}
+
+Room createUpRoom()
+{
+
+	Room ret;
+
+	ret.setMaxX(10);
+
+	ret.setMaxY(10);
+
+	//top wall
+
+	ret.addWalls(0, 0, 0, 9);
+
+	//Left Wall
+
+	ret.addWalls(0, 0, 4, 0);
+
+	ret.addLeftDoor(5, 0);
+
+	ret.addWalls(6, 0, 9, 0);
+
+	//Bottom Wall
+
+	ret.addWalls(9, 0, 9, 4);
+
+	ret.addDownDoor(9, 5);
+
+	ret.addWalls(9, 6, 9, 9);
+
+	//Right Wall
+
+	ret.addWalls(0, 9, 4, 9);
+
+	ret.addRightDoor(5, 9);
+
+	ret.addWalls(6, 9, 9, 9);
+
+	return ret;
+}
+
+Room createDownRoom()
+{
+
+	Room ret;
+
+	ret.setMaxX(10);
+
+	ret.setMaxY(10);
+
+	//Top Wall
+
+	ret.addWalls(0, 0, 0, 4);
+
+	ret.addUpDoor(0, 5);
+
+	ret.addWalls(0, 6, 0, 9);
+
+	//Left Wall
+
+	ret.addWalls(0, 0, 4, 0);
+
+	ret.addLeftDoor(5, 0);
+
+	ret.addWalls(6, 0, 9, 0);
+
+	//Bottom Wall
+
+	ret.addWalls(9, 0, 9, 9);
+
+	//Right Wall
+
+	ret.addWalls(0, 9, 4, 9);
+
+	ret.addRightDoor(5, 9);
+
+	ret.addWalls(6, 9, 9, 9);
+
+	return ret;
+}
+
+//botton Left corner room
+Room createRoom9_0()
+{
+
 	Room ret;
 
 	ret.setMaxX(20);
-	ret.setMaxY(10);
 
-	ret.addWalls(0,0,0,9);
+	ret.setMaxY(20);
 
-	ret.addWalls(1,0,19,0);
-	ret.addWalls(19,1,19,5);
-	ret.addDownDoor(2,2);
-	ret.addWalls(19,7,19,9);
-	ret.addWalls(1,9,4,9);
-	ret.addRightDoor(5,9);
-	ret.addWalls(6,9,18,9);
+	//Left Wall
+
+	ret.addWalls(0, 0, 19, 0);
+
+	//Bottom Wall
+
+	ret.addWalls(19, 0, 19, 19);
+
+	//Top Wall
+
+	ret.addWalls(0, 0, 0, 4);
+
+	ret.addUpDoor(0, 5);
+
+	ret.addWalls(0, 6, 0, 8);
+
+	ret.addWalls(12, 8, 12, 19);
+
+	//Right Wall
+
+	ret.addWalls(0, 8, 12, 8);
+
+	ret.addWalls(12, 19, 16, 19);
+
+	ret.addRightDoor(17, 19);
+
+	ret.addWalls(18, 19, 19, 19);
 
 	return ret;
-
 }
 
-Room createRoom1_0(){
+//botton Right corner room
+Room createRoom9_9()
+{
 
 	Room ret;
-	ret.setMaxX(10);
-	ret.setMaxY(20);
-	ret.addUpDoor(0,4);
-	ret.addWalls(0,0,0,3);
-	ret.addWalls(0,4,0,19);
-	ret.addWalls(0,0,9,0);
-	ret.addWalls(9,0,9,19);
-	ret.addWalls(9,0,9,19);
 
+	ret.setMaxX(30);
 
+	ret.setMaxY(30);
+
+	//Top Wall
+
+	ret.addWalls(0, 0, 0, 7);
+
+	ret.addUpDoor(0, 8);
+
+	ret.addWalls(0, 9, 0, 29);
+
+	//Left Wall
+
+	ret.addWalls(0, 0, 11, 0);
+
+	ret.addLeftDoor(12, 0);
+
+	ret.addWalls(13, 0, 29, 0);
+
+	//Right Wall;
+
+	ret.addWalls(29, 0, 29, 29);
+
+	//Bottom Wall
+
+	ret.addWalls(0, 29, 29, 29);
+
+	return ret;
 }
-Room createRoom0_1(){
 
+//top Right corner room
+
+Room createRoom0_9()
+{
+
+	Room ret;
+
+	ret.setMaxX(25);
+
+	ret.setMaxY(25);
+
+	//top Wall
+
+	ret.addWalls(0, 0, 0, 24);
+
+	ret.addWalls(18, 0, 18, 18);
+
+	//right Wall
+
+	ret.addWalls(0, 24, 24, 24);
+
+	//Left Wall
+
+	ret.addWalls(0, 0, 4, 0);
+
+	ret.addLeftDoor(5, 0);
+
+	ret.addWalls(6, 0, 7, 0);
+
+	ret.addWalls(7, 18, 18, 18);
+
+	ret.addWalls(18, 0, 24, 0);
+
+	// Bottom Wall
+
+	ret.addWalls(24, 0, 24, 3);
+
+	ret.addDownDoor(24, 4);
+
+	ret.addWalls(24, 5, 24, 24);
+
+	ret.addWalls(7, 0, 7, 18);
+
+	return ret;
 }
--- DC/Dungon_Crawler/Dungon_Crawler.h
+++ DC/Dungon_Crawler/Dungon_Crawler.h
@@ -15,3 +15,18 @@ void TestResistanceEffects();
 Room createRoom0_0();
 Room createRoom1_0();
 Room createRoom0_1();
+
+Room createDefaultRoom();
+Room createLeftRoom();
+Room createRightRoom();
+Room createUpRoom();
+Room createDownRoom();
+
+//botton Left corner room
+Room createRoom9_0();
+
+//botton Right corner room
+Room createRoom9_9();
+
+//top Right corner room
+Room createRoom0_9();
